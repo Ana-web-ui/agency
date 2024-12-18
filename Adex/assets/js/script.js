d786ab42f966bf964335842b0d41078dd7c80efd
ana leite <anacarolinaleite1401@gmail.com>
2024-12-18 20:06:43 -0300

    iniciando a parte do feature

--- Adex/assets/js/script.js
+++ Adex/assets/js/script.js
@@ -32,14 +32,13 @@ addEventOnElements(navTogglers, "click", togglerNavBar);
 
 const header = document.querySelector("[data-header]");
 
-window.addEventListener("scroll", function(){
-    if(this.window.scrollY > 100){
-        header.classList.add("active");
-    }else {
-        header.classList.remove("active");
-    }
-})
-
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
 
 /**
  * SLIDER
@@ -47,64 +46,87 @@ window.addEventListener("scroll", function(){
 
 const sliders = document.querySelectorAll("[data-slider]");
 
-const initSLider = function(currentSlider){
+const initSLider = function (currentSlider) {
+  const sliderContainer = currentSlider.querySelector("[data-slider-container");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
 
-    const sliderContainer = currentSlider.querySelector("[data-slider-container");
-    const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
-    const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+  let currentSlidePos = 0;
 
-    let currentSlidePos = 0;
+  const moveSliderItem = function () {
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+  };
 
-    const moveSliderItem = function(){
-        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
-    }
+  /**
+   * NEXT SLIDE
+   */
 
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= sliderContainer.childElementCount - 1;
 
-    /**
-     * NEXT SLIDE
-     */
-
-    const slideNext = function(){
-        const slideEnd = currentSlidePos >= sliderContainer.childElementCount -1;
-
-        if(slideEnd){
-            currentSlidePos = 0;
-        }else {
-            currentSlidePos++;
-        }
-
-        moveSliderItem();
-
-        sliderNextBtn.addEventListener('click', slideNext);
-    }
-
-    
-
-
-/**
-     * PREV SLIDE
-     */
-
-const slidePrev = function(){
-
-    if(currentSlidePos <= 0){
-        currentSlidePos = sliderContainer.childElementCount - 1;
-    }else {
-        currentSlidePos--;
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
     }
 
     moveSliderItem();
 
-    sliderPrevBtn.addEventListener('click', slidePrev);
-};
+    sliderNextBtn.addEventListener("click", slideNext);
+  };
 
+  /**
+   * PREV SLIDE
+   */
 
+  const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+      currentSlidePos = sliderContainer.childElementCount - 1;
+    } else {
+      currentSlidePos--;
+    }
 
-const dontHaveExtraItem = sliderContainer.childElementCount <= 1;
-if (dontHaveExtraItem){
+    moveSliderItem();
+
+    sliderPrevBtn.addEventListener("click", slidePrev);
+  };
+
+  const dontHaveExtraItem = sliderContainer.childElementCount <= 1;
+  if (dontHaveExtraItem) {
     sliderNextBtn.style.display = "none";
     sliderPrevBtn.style.display = "none";
+  }
+
+  for (let i = 0, len = sliders.length; i < len; i++) {
+    initSLider(sliders[i]);
+  }
+};
+
+/**
+ * ACCORDION
+ */
+
+const accordions = document.querySelectorAll("[data-accordion]");
+
+let lastActiveAccordion = accordions[0];
+
+const initAccordion = function (currentAccordion) {
+  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn");
+
+  const expandAccordion = function () {
+    if (lastActiveAccordion && lastActiveAccordion != currentAccordion);
+    {
+      lastActiveAccordion.classList.remove("expanded");
+    }
+
+    currentAccordion.classList.toggle("expanded");
+
+    lastActiveAccordion = currentAccordion;
+  };
+
+  accordionBtn.addEventListener("click", expandAccordion);
+};
+
+for (let i = 0, len = accordions.length; i < len; i++) {
+  initAccordion(accordions[i]);
 }
-
-
-for ( let i = 0, len = sliders.length; i < len; i++){ initSLider(sliders[i])};}
